b01036fae3484d06587dd1efb7d5cbe3d3a8bf34
dhyan2004 <dhyan.work.2815@gmail.com>
2025-10-27 22:48:46 +0530

    update: main file changes

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Visualizer from './pages/Visualizer';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-950">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Visualizer />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
